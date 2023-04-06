3cb1e7dddfa60ee8e21914a3ff926442d6317c05
luc0liv <luoliveiradacunha@gmail.com>
2023-04-06 19:11:48 -0300

    feat: create Blog Post model

---
+++ src/models/BlogPost.js
@@ -0,0 +1,34 @@
+const BlogPostModel = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: { type: DataTypes.STRING, foreignKey: true },
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  },
+  {
+    timestamps: false,
+    underscored: true,
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: 'user_id',
+      as: 'users'
+    });
+    BlogPost.hasMany(models.PostsCategories, {
+      foreignKey: 'post_id',
+      as: 'posts_categories',
+    });
+  }
+
+  return BlogPost;
+};
+
+module.exports = BlogPostModel;
