57d075097ea9fd97bcd92a9c30ef653a86503345
luc0liv <luoliveiradacunha@gmail.com>
2023-04-06 18:48:55 -0300

    feat: create users model

---
+++ src/models/User.js
@@ -0,0 +1,29 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    underscored: true,
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'user_id',
+      as: 'blog_posts'
+    })
+  }
+
+  return User;
+};
+
+module.exports = UserModel;
