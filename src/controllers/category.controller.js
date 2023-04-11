e98fabbe440e4003688ab238a02c3e6f4593c80b
luc0liv <luoliveiradacunha@gmail.com>
2023-04-11 17:14:18 -0300

    feat: create endpoint to get all categories

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -13,6 +13,18 @@ const createCategory = async (req, res) => {
   }
 };
 
+const getAllCategories = async (_req, res) => {
+  try {
+    const categories = await CategoryService.getAllCategories();
+    return res.status(200).json(categories);
+  } catch (error) {
+    return res
+    .status(500)
+    .json({ message: error.message });
+  }
+};
+
 module.exports = {
   createCategory,
+  getAllCategories,
 };
--- src/services/category.service.js
+++ src/services/category.service.js
@@ -1,7 +1,9 @@
 const { Category } = require('../models');
 
 const createCategory = (category) => Category.create({ ...category });
+const getAllCategories = () => Category.findAll();
 
 module.exports = {
   createCategory,
+  getAllCategories,
 };
